4c6e13dcda3dfbdb6c5e93e4511413f431624d10
Kylee Bijerring <kylee24b@gmail.com>
2021-10-03 13:31:23 -0600

    begining

---
+++ react-website-exclusive/src/components/navbar/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Nav, NavbarContainer, NavLogo } from './Navelements'
+
+
+const Navbar = () => {
+    return (
+       
+<Nav>
+<NavbarContainer>
+    <NavLogo to="/">
+        dolla
+    </NavLogo>
+    <h1>hi</h1>
+</NavbarContainer>
+</Nav>
+       
+    );
+};
+
+export default Navbar;
