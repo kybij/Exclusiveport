b65963ca7134f81e995e7a3669df70fb35b8a5b4
Kylee Bijerring <kylee24b@gmail.com>
2021-10-03 22:59:34 -0600

    new

--- react-website-exclusive/src/App.js
+++ react-website-exclusive/src/App.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar';
-import {BrowserRouter as Router } from 'react-router-dom'
-import Home from './components/home'
-import Projects from './components/Projects';
-import Contact from './components/contact';
 import Footer from './components/footer';
 import PortfolioContainer from './components/PortfolioContainer.js';
 function App() {
